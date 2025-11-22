d3d14b7c7c3f630a527ba91d9486f208590f44e2
Asad Mansuri <asadmansuri6797@gmail.com>
2025-11-22 13:14:31 +0530

    Task Module: Migration and Model added

---
+++ app/Models/User/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\User;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'due_date',
+        'user_id'
+    ];
+
+    public function user() { 
+        return $this->belongsTo(User::class);
+    }
+}
